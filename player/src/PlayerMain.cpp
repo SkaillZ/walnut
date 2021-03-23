ecba172b9e27c309e15094ec5235cb2c2dc1761f
SkaillZ <skaillzsupp@gmail.com>
2021-03-23 23:20:29 +0100

    std::chrono and filesystem madness

--- player/src/PlayerMain.cpp
+++ player/src/PlayerMain.cpp
@@ -18,7 +18,9 @@ std::filesystem::path findNewestLibrary(std::string basePath) {
 
   for (const auto& entry: std::filesystem::directory_iterator(basePath)) {
     auto writeTime = std::filesystem::last_write_time(entry.path());
-    auto writeTimeSec = decltype(writeTime)::clock::to_time_t(writeTime);
+    auto writeTimeClock = decltype(writeTime)::clock::now();
+    auto writeTimeSec = std::chrono::time_point_cast<std::chrono::seconds>(writeTimeClock)
+      .time_since_epoch().count();
     if (writeTimeSec > maxWriteTime) {
       maxWriteTime = writeTimeSec;
       newestLibPath = entry.path();
